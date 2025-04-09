415080b14e82c916a26c7a6e80d5bbfc71462905
Nils <nils@treichel76.de>
2025-04-09 12:33:57 +0200

    added satellite struct and configs

--- decode.cpp
+++ decode.cpp
@@ -2,6 +2,21 @@
 #include <fstream>
 #include <vector>
 
+struct SatelliteConfig {
+    int id;
+    int tap1;
+    int tap2;
+    int t;
+};
+
+// Konfiguration aller 24 Satelliten
+std::vector<SatelliteConfig> satelliteConfigs = {
+    {1, 2, 6, 5},   {2, 3, 7, 6},   {3, 4, 8, 7},   {4, 5, 9, 8},   {5, 1, 9, 17},
+    {6, 2, 10, 18}, {7, 1, 8, 139}, {8, 2, 9, 140}, {9, 3, 10, 141}, {10, 2, 3, 251},
+    {11, 3, 4, 252}, {12, 5, 6, 254}, {13, 6, 7, 255}, {14, 7, 8, 256}, {15, 8, 9, 257},
+    {16, 9, 10, 258}, {17, 1, 4, 469}, {18, 2, 5, 470}, {19, 3, 6, 471}, {20, 4, 7, 472},
+    {21, 5, 8, 473}, {22, 6, 9, 474}, {23, 1, 3, 509}, {24, 4, 6, 512}
+};
 
 std::vector<int> readFile(const std::string& filename) {
     std::ifstream inputFile(filename);
